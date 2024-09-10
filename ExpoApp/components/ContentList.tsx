462fab9bf9499353384b306cbfc23b281ff8c3f4
Henrik Johnson <spam@henrik.org>
2024-09-10 09:57:57 -0700

    Try at fixing crash

--- ExpoApp/components/ContentList.tsx
+++ ExpoApp/components/ContentList.tsx
@@ -1,11 +1,10 @@
 import React, {useEffect, useState} from "react";
-import {activeQueue, makeApiCall, MediaItem, query} from "../utils/Api";
+import {MediaItem, query} from "../utils/Api";
 import {FlatList, Keyboard, ListRenderItemInfo, Pressable, View} from "react-native";
 import {ContentItem} from "./ContentItem";
-import {TextInput} from "react-native-paper";
+import {PaperProvider, TextInput, useTheme} from "react-native-paper";
 import {Dropdown, Option} from "react-native-paper-dropdown";
-import {AppContext, useAppContent} from "./AppContex";
-import {getRoom} from "../utils/Storage";
+import {useAppContent} from "./AppContex";
 
 const CRITERIA_OPTIONS : Option[] = [
     "Entered",
@@ -21,6 +20,7 @@ export function ContentList(props: {queue: string}) {
     const [sort, setSort] = useState("Entered" as string | undefined);
     const [refreshing, setRefreshing] = useState(false);
     const appContext = useAppContent();
+    const theme = useTheme();
 
     const [search, setSearch] = useState("")
 
@@ -55,32 +55,34 @@ export function ContentList(props: {queue: string}) {
     }
 
     return (
-        <View>
-            <View style={{flexDirection: "row", columnGap: 2}}>
-                <TextInput style={{flex: 1}} placeholder={"Search"} onChangeText={setSearch} value={search}/>
-                <Dropdown
-                    label=" "
-                    placeholder="Sort"
-                    options={CRITERIA_OPTIONS}
-                    value={sort}
-                    onSelect={setSortFiltered}
+        <PaperProvider theme={theme}>
+            <View>
+                <View style={{flexDirection: "row", columnGap: 2}}>
+                    <TextInput style={{flex: 1}} placeholder={"Search"} onChangeText={setSearch} value={search}/>
+                    <Dropdown
+                        label=" "
+                        placeholder="Sort"
+                        options={CRITERIA_OPTIONS}
+                        value={sort}
+                        onSelect={setSortFiltered}
+                    />
+                </View>
+                <FlatList
+                    data={data}
+                    renderItem={renderItem}
+                    onScrollBeginDrag={() => {
+                        Keyboard.dismiss();
+                    }}
+                    refreshing={refreshing}
+                    onRefresh={() => {
+                        setRefreshing(true);
+                        fetchContents(true);
+                    }}
+                    onEndReached={() => fetchContents(false)}
+                    onEndReachedThreshold={0.5}
+                    keyExtractor={(item: MediaItem) => String(item.itemId)}
                 />
             </View>
-            <FlatList
-                data={data}
-                renderItem={renderItem}
-                onScrollBeginDrag={() => {
-                    Keyboard.dismiss();
-                }}
-                refreshing={refreshing}
-                onRefresh={() => {
-                    setRefreshing(true);
-                    fetchContents(true);
-                }}
-                onEndReached={() => fetchContents(false)}
-                onEndReachedThreshold={0.5}
-                keyExtractor={(item: MediaItem) => String(item.itemId)}
-            />
-        </View>
-    );
-};
+        </PaperProvider>
+    )
+}
