3b2e3aebda8dbd63350dee359344f0016e03a7a1
Henrik Johnson <spam@henrik.org>
2024-11-15 17:55:24 -0800

    More tweaks to repeating button

--- ExpoApp/utils/RepeatingButton.ts
+++ ExpoApp/utils/RepeatingButton.ts
@@ -5,7 +5,8 @@ const INITIAL_DELAY = 100;
 const SECONDARY_DELAY = 300;
 const REPEAT_INTERVAL = 50;
 
-let currentAction: string | undefined;
+let repeatingAction : string | undefined = undefined;
+let nextActionId = 0;
 let currentTimerId = -1;
 let currentIntervalId = -1;
 
@@ -17,22 +18,24 @@ function stopSending() {
 
     currentTimerId = -1;
     currentIntervalId = -1;
-    currentAction = undefined;
+    nextActionId++;
+    repeatingAction = undefined;
 }
 
 export function startSendingAction(appContext: AppContext, action: string) {
     console.log("startSendingAction", action);
     stopSending();
-    currentAction = action;
+    let currentActionId = ++nextActionId;
+    repeatingAction = action;
 
     setTimeout(() => {
         apiSend(appContext, action);
 
-        if (currentAction === action && currentTimerId < 0) {
+        if (nextActionId === currentActionId && currentTimerId < 0) {
             currentTimerId = setTimeout(() => {
-                if (currentAction === action && currentIntervalId < 0) {
+                if (nextActionId === currentActionId && currentIntervalId < 0) {
                     currentIntervalId = setInterval(() => {
-                        if (currentAction === action) {
+                        if (nextActionId === currentActionId) {
                             apiIdle(appContext, action);
                         }
                     }, REPEAT_INTERVAL) as unknown as number;
@@ -44,7 +47,7 @@ export function startSendingAction(appContext: AppContext, action: string) {
 
 export function stopSendingAction(appContext: AppContext, action: string) {
     console.log("stopSendingAction", action);
-    if (action === currentAction) {
+    if (repeatingAction === action) {
         stopSending();
     }
 }
